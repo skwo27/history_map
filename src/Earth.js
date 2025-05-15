9a8df50357be51dc5405661d4190f947912af55e
skwo27 <165741912+skwo27@users.noreply.github.com>
2025-05-15 09:48:19 +0900

    feat:: 커밋 실수로 다 날려먹은거 복구

---
+++ src/Earth.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { TextureLoader } from "three";
+import { useLoader } from "@react-three/fiber";
+import earth from "/Users/dgsw48/WebstormProjects/untitled18/src/earthhd.jpg";
+
+export function Earth() {
+    const texture = useLoader(TextureLoader,  earth ); // public 폴더에 넣어야 함
+
+    return (
+        <mesh>
+            <sphereGeometry args={[2, 64, 64]} />
+            <meshStandardMaterial map={texture} />
+        </mesh>
+    );
+}
+
+export default function EarthScene() {
+    return (
+        <Canvas camera={{ position: [0, 0, 5] }}>
+            <ambientLight />
+            <directionalLight position={[5, 5, 5]} />
+            <OrbitControls enableZoom={true} />
+            <Earth />
+        </Canvas>
+    );
+}
