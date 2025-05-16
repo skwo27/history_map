8678387ea8cf180de887d31a6eaf4360d3175a0e
skwo27 <165741912+skwo27@users.noreply.github.com>
2025-05-16 09:54:22 +0900

    feat:: 기능추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// App.js
 import EarthScene from "./components/Earth";
+import Year from "./components/year";
 
-function App() {
+export default function App() {
     return (
-        <div style={{ width: "100vw", height: "100vh" }}>
+        <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
             <EarthScene />
+
+            {/* 화면 고정 텍스트 */}
+            <div style={{
+                position: "absolute",
+                top: "20px",
+                left: "20px",
+                color: "white",
+                fontSize: "24px",
+                fontWeight: "bold",
+                zIndex: 10
+            }}>
+                <Year/>
+            </div>
         </div>
     );
 }
-
-export default App;
--- src/components/Earth.js
+++ src/components/Earth.js
@@ -4,6 +4,7 @@ import { OrbitControls } from "@react-three/drei";
 import { TextureLoader } from "three";
 import { useLoader } from "@react-three/fiber";
 import earth from "/Users/dgsw48/WebstormProjects/untitled18/src/components/earthhd.jpg";
+import Year from "./year";
 
 export function Earth() {
     const texture = useLoader(TextureLoader,  earth ); // public 폴더에 넣어야 함
@@ -22,11 +23,11 @@ export default function EarthScene() {
             <ambientLight />
             <directionalLight position={[5, 5, 5]} />
             <OrbitControls enableZoom={true}
-                           enablePan={false}         // 화면 끌어당기기 막음
+                           // enablePan={false}         // 화면 끌어당기기 막음
                            enableRotate={true}      // 회전만 허용
                            autoRotate={false}       // 필요 시 true로 설정
                            target={[0, 0, 0]}
-                           minDistance={3}
+                           minDistance={2.8}
                            maxDistance={10}
             />
             <Earth />
